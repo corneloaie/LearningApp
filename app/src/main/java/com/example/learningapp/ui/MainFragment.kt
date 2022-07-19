f6d16b5f482fab3c01be2533a5696e7b5d94192a
Cornel Oaie <cornel.oaie@thoughtworks.com>
2022-07-19 13:53:54 +0300

    added temp files

---
+++ app/src/main/java/com/example/learningapp/ui/MainFragment.kt
@@ -0,0 +1,13 @@
+package com.example.learningapp.ui
+
+import androidx.fragment.app.activityViewModels
+import com.example.learningapp.R
+import com.example.learningapp.base.view.fragment.BaseFragment
+import com.example.learningapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : BaseFragment<MainViewModel, ActivityMainBinding>() {
+    override val layoutId: Int = R.layout.activity_main
+    override val viewModel: MainViewModel by activityViewModels()
+}
