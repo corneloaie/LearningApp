df8623807230bc7bc381074b0a273fe09cddd0cc
dumitrescucatalinandrei <andrei.dumitrescu@thoughtworks.com>
2022-08-24 12:39:58 +0300

    Modified the API call to only return collections.

--- app/src/main/java/com/example/learningapp/data/remote/ElrondService.kt
+++ app/src/main/java/com/example/learningapp/data/remote/ElrondService.kt
@@ -5,10 +5,11 @@ import com.example.learningapp.domain.model.Nft
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ElrondService {
-    @GET("/nfts?size=25")
-    fun getNfts(): Single<List<NftDto>>
+    @GET("/nfts")
+    fun getNfts(@Query("collection") getByCollection: String = "ELFARM-327070"): Single<List<NftDto>>
     @GET("/nfts/{id}")
     fun getNft(@Path("id") getById:String) : Single<NftDto>
 }
